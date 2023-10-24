881a08d13900248e24f8da62a62470a4fa5c5dee
chicattita <chantaka52@gmail.com>
2023-10-24 14:26:15 +0900

    自動デプロイ（main）変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,4 @@
+# config/deploy.rb
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
 lock '3.18.0'
 
@@ -23,7 +24,7 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 # Unicornの設定ファイルの場所
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
-
+set :branch, 'main'
 # デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
